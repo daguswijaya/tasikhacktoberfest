c33525b3c60ab61223dcaee96d389b8bfe43f251
Dagus Wijaya <dagus.wijaya@gmail.com>
2019-10-05 10:50:44 +0700

    First commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tasikmalaya Hacktoberfest 2019</title>
+</head>
+<body>
+<center><img src="/assets/img/hacktoberfest2019.png" alt="Tasikmalaya Hacktoberfest 2019"></center>
+</body>
+</html>
